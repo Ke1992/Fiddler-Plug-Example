e03e7548f48b6a9ea18ec77d35d6f518a128db48
Ke1992 <740757242@qq.com>
2019-01-17 17:07:03 +0800

    添加 001 Hello World
    Signed-off-by: Ke1992 <740757242@qq.com>

---
+++ 001 Hello World/Main.cs
@@ -0,0 +1,21 @@
+﻿using Fiddler;
+using System.Windows.Forms;
+
+namespace _001_Hello_World
+{
+    public class Main:  UserControl, IFiddlerExtension
+    {
+        public void OnBeforeUnload()
+        { }
+
+        public void OnLoad()
+        {
+            //新建一个Fiddler插件的page
+            TabPage page = new TabPage("Heelo World");
+            //将page加入Fiddler的tab选项卡中
+            FiddlerApplication.UI.tabsViews.TabPages.Add(page);
+            //输出Hello World
+            FiddlerApplication.DoNotifyUser("Hello", "Hello World");
+        }
+    }
+}
