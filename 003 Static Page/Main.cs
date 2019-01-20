6fd706bbed26e6a3a4dcc8f7a8864ef8aa431311
Ke1992 <740757242@qq.com>
2019-01-20 21:21:33 +0800

    添加 003 Static Page
    Signed-off-by: Ke1992 <740757242@qq.com>

---
+++ 003 Static Page/Main.cs
@@ -0,0 +1,34 @@
+﻿using Fiddler;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace _003_Static_Page
+{
+    public class Main : UserControl, IFiddlerExtension
+    {
+        public static Container container;
+
+        public void OnBeforeUnload()
+        { }
+
+        public void OnLoad()
+        {
+            //新建UI对象
+            container = new Container();
+
+            //新建一个Fiddler插件的page
+            TabPage page = new TabPage("PlugExample");
+            //将page加入Fiddler的tab选项卡中
+            FiddlerApplication.UI.tabsViews.TabPages.Add(page);
+
+            //将WinForm和WPF联系起来(在WinForm中调用WPF)
+            ElementHost element = new ElementHost();
+            element.Child = container;
+            element.Dock = DockStyle.Fill;
+
+            //将WPF挂载对象添加到page中
+            page.Controls.Add(element);
+        }
+    }
+}
+
